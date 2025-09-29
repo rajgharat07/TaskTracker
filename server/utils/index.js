4af377f55115c95b3d04dd9611ad073f4cd3abfd
rajgharat07 <gharatyashraj@gmail.com>
2025-09-29 12:37:08 +0530

    fixing secure:true

--- server/utils/index.js
+++ server/utils/index.js
@@ -21,7 +21,7 @@ export const createJWT = (res, userId) => {
     // Change sameSite from strict to none when you deploy your app
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
+      secure: true,
       sameSite: "none", //prevent CSRF attack
       maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
       path: '/',
